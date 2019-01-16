9d71bfe20ea1a7430b9bec2ddd15c5cc1d745095
David Scott Perez <david.scott.perez@live.com>
2019-01-15 21:33:00 -0800

    about page added and updated with content

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -59,7 +59,7 @@ class Login extends Component {
         <div className="login-box">
           <div className="row">
    
-            <div className="col-md-8 m-auto">
+            <div className="col-md-6 m-auto">
            
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">
--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -7,36 +7,43 @@ import FadeIn from "react-fade-in";
 class About extends Component {
   render() {
     return (
-      <div className="landing">
+      <div className="about-whole-page">
+      <div className="about">
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
             <div className="row">
-              <div className="col-md-12 text-center">
+              <div className="col-sm-12">
                 <FadeIn>
-                  <h1 className="display-3 mb-4 brand-text">OVERWATCH</h1>
+                  <h1 className="display-3 mb-4 brand-text">Simple & Effective</h1>
                   <p className="lead">
                     {" "}
-                   About stuff
+                  Customer organization made easy.
                   </p>
-                  <hr />
-                  <div>
-                    {/* <Link
-                      to="/register"
-                      className="btn btn-lg btn-primary mr-2"
-                    >
-                      Register
-                    </Link> */}
-                    <br />
-                    <Link to="/login" className="btn btn-lg btn-outline-light login-button">
-                      Login
-                    </Link>
-                  </div>
                 </FadeIn>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="about-2">
+      </div>
+      <div className="about-3">
+      <FadeIn>
+      <div>
+                    <Link
+                      to="/register"
+                      className="btn btn-lg btn-outline-light mr-2 login-button">
+                      Register
+                    </Link>
+                    <br />
+                    <Link to="/login" className="btn btn-lg btn-outline-light login-button">
+                      Login
+                    </Link>
+                  </div>
+                  </FadeIn>
+      </div>
+      
+    </div>
     );
   }
 }
